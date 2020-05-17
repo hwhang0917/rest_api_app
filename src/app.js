61d4906026719108da4a5b0543d2ca6ae751140e
Mark Whang <hwhang0917@gmail.com>
2020-05-18 03:47:04 +0900

    Add imported middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
 import express from "express";
+import path from "path";
+import helmet from "helmet";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
 
 const app = express();
 
+const CookieStore = MongoStore(session);
+
+// View Engine and Static
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+// Imported Middlewares
+app.use(helmet());
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.render("landing");
 });
 
 export default app;
